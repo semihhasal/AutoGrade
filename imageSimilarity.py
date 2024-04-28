90a8c5e80ac8ad98fbbfb5ac1b2be0bfcfde1b01
Dogukan Sahin <dogukansahin551@gmail.com>
2024-04-28 23:37:27 +0300

    110 datasets added

--- imageSimilarity.py
+++ imageSimilarity.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 from skimage.metrics import structural_similarity as ssim
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.applications.vgg16 import VGG16, preprocpre_process_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import Model
 
